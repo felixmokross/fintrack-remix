7b7aebc2c420fd1e0b6623768111fb17696e2e5d
Felix Mokross <felixmokross@hey.com>
2022-07-19 20:33:41 +0200

    Testing if Cypress issue is due to React issue with hydration in combination with tags injected into head changed by Cypress

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,20 +1,25 @@
-import * as React from "react";
+// import * as React from "react";
 import { RemixBrowser } from "@remix-run/react";
-import { hydrateRoot } from "react-dom/client";
+// import { hydrateRoot } from "react-dom/client";
+console.log(`env: ${process.env.NODE_ENV}`);
+// if (process.env.NODE_ENV === "test") {
+require("react-dom").hydrate(<RemixBrowser />, document);
+// } else {
+//   hydrateRoot(document, <RemixBrowser />);
+// }
+// function hydrate() {
+//   React.startTransition(() => {
+//     hydrateRoot(
+//       document,
+//       <React.StrictMode>
+//         <RemixBrowser />
+//       </React.StrictMode>
+//     );
+//   });
+// }
 
-function hydrate() {
-  React.startTransition(() => {
-    hydrateRoot(
-      document,
-      <React.StrictMode>
-        <RemixBrowser />
-      </React.StrictMode>
-    );
-  });
-}
-
-if (window.requestIdleCallback) {
-  window.requestIdleCallback(hydrate);
-} else {
-  window.setTimeout(hydrate, 1);
-}
+// if (window.requestIdleCallback) {
+//   window.requestIdleCallback(hydrate);
+// } else {
+//   window.setTimeout(hydrate, 1);
+// }
