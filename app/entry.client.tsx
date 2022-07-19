d093759dcc9da5fa418956e86a247f1650e65ad3
Felix Mokross <felixmokross@hey.com>
2022-07-19 21:28:21 +0200

    Switch to npm for simplicity (making sure lock file is used everywhere)

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,25 +1,20 @@
-// import * as React from "react";
+import * as React from "react";
 import { RemixBrowser } from "@remix-run/react";
-// import { hydrateRoot } from "react-dom/client";
-console.log(`env: ${process.env.NODE_ENV}`);
-// if (process.env.NODE_ENV === "test") {
-require("react-dom").hydrate(<RemixBrowser />, document);
-// } else {
-//   hydrateRoot(document, <RemixBrowser />);
-// }
-// function hydrate() {
-//   React.startTransition(() => {
-//     hydrateRoot(
-//       document,
-//       <React.StrictMode>
-//         <RemixBrowser />
-//       </React.StrictMode>
-//     );
-//   });
-// }
+import { hydrateRoot } from "react-dom/client";
 
-// if (window.requestIdleCallback) {
-//   window.requestIdleCallback(hydrate);
-// } else {
-//   window.setTimeout(hydrate, 1);
-// }
+function hydrate() {
+  React.startTransition(() => {
+    hydrateRoot(
+      document,
+      <React.StrictMode>
+        <RemixBrowser />
+      </React.StrictMode>
+    );
+  });
+}
+
+if (window.requestIdleCallback) {
+  window.requestIdleCallback(hydrate);
+} else {
+  window.setTimeout(hydrate, 1);
+}
