544c90f2f00b72af4979b480778190258b1b1ce1
Felix Mokross <felixmokross@hey.com>
2022-07-22 21:03:36 +0200

    Add basic nav bar

---
+++ app/routes/__app/settings/index.tsx
@@ -0,0 +1,8 @@
+import type { LoaderFunction } from "@remix-run/server-runtime";
+import { redirect } from "@remix-run/server-runtime";
+import { requireUserId } from "~/session.server";
+
+export const loader: LoaderFunction = async ({ request }) => {
+  await requireUserId(request);
+  return redirect("/settings/asset-classes");
+};
