dfb09f79b47c173e562072cd996fc863f2b38e42
Felix Mokross <felixmokross@gmail.com>
2022-08-05 22:19:51 +0200

    Finish import logic

--- app/import/types.ts
+++ app/import/types.ts
@@ -72,3 +72,72 @@ export interface ExpenseCategory {
   _id?: ObjectId;
   name: string;
 }
+
+export interface Transaction {
+  _id?: ObjectId;
+  date: Date;
+  note?: string;
+  bookings: readonly Booking[];
+}
+
+export type Booking =
+  | Charge
+  | Deposit
+  | Income
+  | Expense
+  | Appreciation
+  | Depreciation;
+interface CommonBooking {
+  type: BookingType;
+}
+
+export interface Charge extends CommonBooking {
+  type: BookingType.CHARGE;
+  note?: string;
+  accountId: ObjectId;
+  unit: AccountUnit;
+  amount: Decimal128;
+}
+
+export interface Deposit extends CommonBooking {
+  type: BookingType.DEPOSIT;
+  note?: string;
+  accountId: ObjectId;
+  unit: AccountUnit;
+  amount: Decimal128;
+}
+
+export interface Income extends CommonBooking {
+  type: BookingType.INCOME;
+  note?: string;
+  incomeCategoryId: ObjectId;
+  currency: string;
+  amount: Decimal128;
+}
+
+export interface Expense extends CommonBooking {
+  type: BookingType.EXPENSE;
+  note?: string;
+  expenseCategoryId: ObjectId;
+  currency: string;
+  amount: Decimal128;
+}
+
+export interface Appreciation extends CommonBooking {
+  type: BookingType.APPRECIATION;
+  amount: Decimal128;
+}
+
+export interface Depreciation extends CommonBooking {
+  type: BookingType.DEPRECIATION;
+  amount: Decimal128;
+}
+
+export enum BookingType {
+  DEPOSIT = "DEPOSIT",
+  CHARGE = "CHARGE",
+  INCOME = "INCOME",
+  EXPENSE = "EXPENSE",
+  APPRECIATION = "APPRECIATION",
+  DEPRECIATION = "DEPRECIATION",
+}
--- app/routes/__app/settings/import.tsx
+++ app/routes/__app/settings/import.tsx
@@ -1,8 +1,10 @@
 import type {
   Account,
   AssetClass,
+  Booking,
   IncomeExpenseCategory,
   Stock,
+  Transaction,
 } from "@prisma/client";
 import { AccountUnit } from "@prisma/client";
 import { IncomeExpenseCategoryType } from "@prisma/client";
@@ -27,8 +29,12 @@ import type {
   IncomeCategory as ImportIncomeCategory,
   ExpenseCategory as ImportExpenseCategory,
   Account as ImportAccount,
+  Transaction as ImportTransaction,
 } from "~/import/types";
-import { AccountUnitKind as ImportAccountUnitKind } from "~/import/types";
+import {
+  BookingType as ImportBookingType,
+  AccountUnitKind as ImportAccountUnitKind,
+} from "~/import/types";
 import { getDb } from "~/mongodb.server";
 import { getSession, requireUserId, sessionStorage } from "~/session.server";
 
@@ -88,59 +94,146 @@ export const action: ActionFunction = async ({ request }) => {
 
   const stocks = Array.from(stocksByImportId.values());
 
-  const incomeCategories = (
-    await db
-      .collection<ImportIncomeCategory>("incomeCategories")
-      .find()
-      .toArray()
-  ).map<Omit<IncomeExpenseCategory, "id" | "createdAt" | "updatedAt">>((c) => ({
-    name: c.name,
-    type: IncomeExpenseCategoryType.INCOME,
-    userId,
-  }));
+  const incomeCategoriesByImportId = new Map(
+    (
+      await db
+        .collection<ImportIncomeCategory>("incomeCategories")
+        .find()
+        .toArray()
+    ).map<[string, Omit<IncomeExpenseCategory, "createdAt" | "updatedAt">]>(
+      (c) => [
+        c._id.toHexString(),
+        {
+          id: cuid(),
+          name: c.name,
+          type: IncomeExpenseCategoryType.INCOME,
+          userId,
+        },
+      ]
+    )
+  );
 
-  const expenseCategories = (
-    await db
-      .collection<ImportExpenseCategory>("expenseCategories")
-      .find()
-      .toArray()
-  ).map<Omit<IncomeExpenseCategory, "id" | "createdAt" | "updatedAt">>((c) => ({
-    name: c.name,
-    type: IncomeExpenseCategoryType.EXPENSE,
-    userId,
-  }));
+  const incomeCategories = Array.from(incomeCategoriesByImportId.values());
 
-  const accounts = (
-    await db.collection<ImportAccount>("accounts").find().toArray()
-  )
-    .map<Omit<Account, "id" | "createdAt" | "updatedAt">>((a) => ({
-      name: a.name,
-      type: a.categoryType,
-      assetClassId: assetClassesByImportId.get(a.categoryId.toHexString())!.id,
-      unit: a.unit.kind,
-      groupId: null,
-      currency:
-        a.unit.kind === ImportAccountUnitKind.CURRENCY ? a.unit.currency : null,
-      stockId:
-        a.unit.kind === ImportAccountUnitKind.STOCK
-          ? stocksByImportId.get(a.unit.stockId.toHexString())!.id
-          : null,
-      preExisting: !a.openingDate,
-      balanceAtStart: a.openingBalance
-        ? new Decimal(a.openingBalance.toString())
-        : null,
-      openingDate: a.openingDate || null,
-      closingDate: a.closingDate || null,
-      isActive: a.isActive,
-      userId,
-    }))
-    .filter(
-      (a) =>
-        a.unit !== AccountUnit.CURRENCY ||
-        !["ETH", "BTC", "ADA", "BCH"].includes(a.currency!)
-    );
+  const expenseCategoriesByImportId = new Map(
+    (
+      await db
+        .collection<ImportExpenseCategory>("expenseCategories")
+        .find()
+        .toArray()
+    ).map<[string, Omit<IncomeExpenseCategory, "createdAt" | "updatedAt">]>(
+      (c) => [
+        c._id.toHexString(),
+        {
+          id: cuid(),
+          name: c.name,
+          type: IncomeExpenseCategoryType.EXPENSE,
+          userId,
+        },
+      ]
+    )
+  );
+
+  const expenseCategories = Array.from(expenseCategoriesByImportId.values());
+
+  const accountsByImportId = new Map(
+    (await db.collection<ImportAccount>("accounts").find().toArray())
+      .map<[string, Omit<Account, "createdAt" | "updatedAt">]>((a) => [
+        a._id.toHexString(),
+        {
+          id: cuid(),
+          name: a.name,
+          type: a.categoryType,
+          assetClassId: assetClassesByImportId.get(a.categoryId.toHexString())!
+            .id,
+          unit: a.unit.kind,
+          groupId: null,
+          currency:
+            a.unit.kind === ImportAccountUnitKind.CURRENCY
+              ? a.unit.currency
+              : null,
+          stockId:
+            a.unit.kind === ImportAccountUnitKind.STOCK
+              ? stocksByImportId.get(a.unit.stockId.toHexString())!.id
+              : null,
+          preExisting: !a.openingDate,
+          balanceAtStart: a.openingBalance
+            ? new Decimal(a.openingBalance.toString())
+            : null,
+          openingDate: a.openingDate || null,
+          closingDate: a.closingDate || null,
+          isActive: a.isActive,
+          userId,
+        },
+      ])
+      .filter(
+        ([, a]) =>
+          a.unit !== AccountUnit.CURRENCY ||
+          !["ETH", "BTC", "ADA", "BCH"].includes(a.currency!)
+      )
+  );
+  const accounts = Array.from(accountsByImportId.values());
+
+  const importTransactions = await db
+    .collection<ImportTransaction>("transactions")
+    .find()
+    .toArray();
+
+  const transactionsByImportId = new Map(
+    importTransactions.map<
+      [string, Omit<Transaction, "createdAt" | "updatedAt" | "bookings">]
+    >((t) => [
+      t._id.toHexString(),
+      {
+        id: cuid(),
+        date: t.date,
+        note: t.note || null,
+        userId,
+      },
+    ])
+  );
+
+  const transactions = Array.from(transactionsByImportId.values());
+
+  const bookings = importTransactions
+    .map<Omit<Booking, "id">[]>((t) =>
+      t.bookings.map((b, index) => ({
+        type: b.type,
+        note:
+          b.type !== ImportBookingType.APPRECIATION &&
+          b.type !== ImportBookingType.DEPRECIATION
+            ? b.note || null
+            : null,
+        accountId:
+          b.type === ImportBookingType.DEPOSIT ||
+          b.type === ImportBookingType.CHARGE
+            ? accountsByImportId.get(b.accountId.toHexString())?.id || null
+            : null,
+        incomeExpenseCategoryId:
+          b.type === ImportBookingType.EXPENSE
+            ? expenseCategoriesByImportId.get(
+                b.expenseCategoryId.toHexString()
+              )!.id
+            : b.type === ImportBookingType.INCOME
+            ? incomeCategoriesByImportId.get(b.incomeCategoryId.toHexString())!
+                .id
+            : null,
+        currency:
+          b.type === ImportBookingType.EXPENSE ||
+          b.type === ImportBookingType.INCOME
+            ? b.currency
+            : null,
+        amount: new Decimal(b.amount.toString()),
+        sortOrder: index,
+        transactionId: transactionsByImportId.get(t._id.toHexString())!.id,
+        userId,
+      }))
+    )
+    .flat();
 
   await prisma.$transaction([
+    prisma.booking.deleteMany({ where: { userId } }),
+    prisma.transaction.deleteMany({ where: { userId } }),
     prisma.account.deleteMany({ where: { userId } }),
     prisma.assetClass.deleteMany({ where: { userId } }),
     prisma.stock.deleteMany({ where: { userId } }),
@@ -152,12 +245,14 @@ export const action: ActionFunction = async ({ request }) => {
     prisma.assetClass.createMany({ data: assetClasses }),
     prisma.stock.createMany({ data: stocks }),
     prisma.account.createMany({ data: accounts }),
+    prisma.transaction.createMany({ data: transactions }),
+    prisma.booking.createMany({ data: bookings }),
   ]);
 
   const session = await getSession(request);
   session.flash(
     "flashMessage",
-    `Imported successful: ${assetClasses.length} asset classes, ${stocks.length} stocks, ${incomeCategories.length} income categories, ${expenseCategories.length} expense categories, ${accounts.length} accounts`
+    `Imported successful: ${assetClasses.length} asset classes, ${stocks.length} stocks, ${incomeCategories.length} income categories, ${expenseCategories.length} expense categories, ${accounts.length} accounts, ${transactions.length} transactions, ${bookings.length} bookings`
   );
 
   return redirect("/settings/import", {
